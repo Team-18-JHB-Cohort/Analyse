1cd097f73b6fab3d32b519c28a4115e081bbb446
Mpho-Marufu <60389363+Mpho-Marufu@users.noreply.github.com>
2020-02-14 09:24:22 +0200

    Added docstring

--- AnalysePredict/stop_words_remover.py
+++ AnalysePredict/stop_words_remover.py
@@ -2,9 +2,20 @@
 # coding: utf-8
 
 # In[3]:
-"""
 
-"""
+ """
+   Should split string in a list in rows
+   Should remove stop_words in the dictionary from the dataframe
+   Should create a new column labeled "without_stop_words"
+    
+    Args:
+        DataFrame
+    
+    Returns:
+        a culumn labeled "Without_Stop_words" with stop words removed.
+    
+     
+    """
 
 def stop_words_remover(df):
     
