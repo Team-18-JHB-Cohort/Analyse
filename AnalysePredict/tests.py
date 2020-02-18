8f78d98cbb9e4a19ae83956414ff37e012bcc392
Lawrence Hlapa <59865367+LawrenceHlapa@users.noreply.github.com>
2020-02-18 13:01:12 +0200

    Added test samples.

--- AnalysePredict/tests.py
+++ AnalysePredict/tests.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import unittest
-from . import dictionary_of_metrics, five_num_summary
+from . import dictionary_of_metrics, five_num_summary, five_num_summary, date_parser, extract_municipality_hashtags, number_of_tweets_per_day, word_splitter, stop_words_remover
 
 class Tests(unittest.TestCase):
 
@@ -13,3 +19,19 @@ class Tests(unittest.TestCase):
 
     def test_five_num_summary(self):
         self.assertEqual(five_num_summary([1, 2, 3, 4, 5, 6]), {'max': 6.0, 'median': 3.5, 'min': 1.0, 'q1': 2.0, 'q3': 5.0})
+
+    def date_parser(list_dates):
+        self.assertEqual(date_parser(dates[-3:]) == ['2019-11-20', '2019-11-20', '2019-11-20'])
+        
+    def extract_municipality_hashtags(df):
+        self.assertEqual(extract_municipality_hashtags(twitter_df.copy()))
+        
+    def number_of_tweets_per_day(df):
+        self.assertEqual(number_of_tweets_per_day(twitter_df.copy()))
+    
+    def word_splitter(df):
+        self.assertEqual(word_splitter(twitter_df.copy()))
+    
+    def stop_words_remover(df):
+        self.assertEqual(stop_words_remover(twitter_df.copy()))
+
