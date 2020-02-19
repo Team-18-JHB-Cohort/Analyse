5a52090a184dba65c432eebe959aa0d96ca4ee15
Marcus Moeng <marcusmoeng@yahoo.com>
2020-02-19 15:01:54 +0200

    Updating the setup.py file

--- AnalysePredict/tests.py
+++ AnalysePredict/tests.py
@@ -5,7 +5,7 @@
 
 
 import unittest
-from . import dictionary_of_metrics, five_num_summary, five_num_summary, date_parser, extract_municipality_hashtags, number_of_tweets_per_day, word_splitter, stop_words_remover
+from AnalysePredict import dictionary_of_metrics, five_num_summary, five_num_summary, date_parser, extract_municipality_hashtags, number_of_tweets_per_day, word_splitter, stop_words_remover
 
 class Tests(unittest.TestCase):
 
@@ -22,17 +22,16 @@ class Tests(unittest.TestCase):
 
     def date_parser(list_dates):
         self.assertEqual(date_parser(dates[-3:]), ['2019-11-20', '2019-11-20', '2019-11-20'])
-        
+
    def extract_municipality_hashtags(self):
-        self.assertEqual(extract_municipality_hashtags(df['municipality'].iloc[5:9]), ['Johannesburg', 'NaN', 'NaN','NaN']) 
+        self.assertEqual(extract_municipality_hashtags(df['municipality'].iloc[5:9]), ['Johannesburg', 'NaN', 'NaN','NaN'])
         self.assertEqual(extract_municipality_hashtags(df['hashtags'].iloc[5:9]), ['NaN', ['#fridaymotivation', '#eskomexpoisf'],['#eskommpumalanga'],'NaN'])
-        
+
     def number_of_tweets_per_day(df):
         self.assertEqual(number_of_tweets_per_day(twitter_df.copy()).loc['2019-11-22', 'Tweets']), '25')
-    
+
     def word_splitter(df):
         self.assertEqual(word_splitter(word_splitter(twitter_df.copy()).loc[37, "Split Tweets"],  ['rt', '@overstrandmuni:', 'we', 'are', 'currently', 'experiencing', 'a', 'power', 'outage', 'in', 'pearly', 'beach', 'due', 'to', 'a', 'cable', 'fault.', 'power', 'is', 'estimated', 'to', 'be', 'restored', 'a…']
-    
+
     def stop_words_remover(df):
         self.assertEqual(stop_words_remover(twitter_df.copy()).loc[0, "Without Stop Words"], ['@bongadlulane', 'send', 'email', 'mediadesk@eskom.co.za'])
-
