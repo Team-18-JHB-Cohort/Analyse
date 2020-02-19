411588765e86e1e5f90ee30efb412c4a4b1c1331
Marcus Moeng <marcusmoeng@yahoo.com>
2020-02-19 16:36:24 +0200

    Restructuring the github repository

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="Analyse",
     version="0.0.1",
@@ -11,7 +8,7 @@ setuptools.setup(
     install_requires = ['numpy', 'pandas'],
     author_email="marcusmoeng@yahoo.com",
     description="This package contains metric functions",
-    long_description=long_description,
+    long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Team-18-JHB-Cohort/Analyse",
     packages=setuptools.find_packages(exclude=['tests*'])
