6c241f3195ec1c0611b7a5c4edce3f6df4e023ff
Marcus Moeng <marcusmoeng@yahoo.com>
2020-02-19 13:19:35 +0200

    Updating the setup.py
    correcting the URL entry

--- AnalysePredict/setup.py
+++ AnalysePredict/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="This package contains metric functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Team-18-JHB-Cohort/Analyse.git",
+    url="https://github.com/Team-18-JHB-Cohort/Analyse/tree/master/AnalysePredict",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
