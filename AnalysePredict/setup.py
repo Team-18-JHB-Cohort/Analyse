32c4db79dfb1040ff7ef66fbaa533e3d814f54b2
Marcus Moeng <marcusmoeng@yahoo.com>
2020-02-19 16:15:36 +0200

    updated the setup.py and deleted dist files

--- AnalysePredict/setup.py
+++ AnalysePredict/setup.py
@@ -4,17 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="AnalysePredict",
+    name="Analyse",
     version="0.0.1",
+    license = 'MIT',
     author="Team_18_JHB_2020",
+    install_requires = ['numpy', 'pandas'],
     author_email="marcusmoeng@yahoo.com",
     description="This package contains metric functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Team-18-JHB-Cohort/Analyse",
-    packages=setuptools.find_packages(except=[tests*]),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
+    packages=setuptools.find_packages(exclude=['tests*'])
 )
