eb2a714a8e9f536ece5239a2e4f807fb4473d13b
Akhona-Stafane <60341376+Akhona-Stafane@users.noreply.github.com>
2020-02-19 11:02:53 +0200

    Added Test cases for function four

--- tests/tests.py
+++ tests/tests.py
@@ -13,3 +13,7 @@ class Tests(unittest.TestCase):
 
     def test_five_num_summary(self):
         self.assertEqual(five_num_summary([1, 2, 3, 4, 5, 6]), {'max': 6.0, 'median': 3.5, 'min': 1.0, 'q1': 2.0, 'q3': 5.0})
+    
+    def extract_municipality_hashtags(self):
+        self.assertEqual(extract_municipality_hashtags(df1['municipality'].iloc[5,9]), ['Johannesburg', 'NaN', 'NaN','NaN']) 
+        self.assertEqual(extract_municipality_hashtags(df1['hashtags'].iloc[5:9]), [NaN, ['#fridaymotivation', '#eskomexpoisf'],['#eskommpumalanga'],'NaN'])
