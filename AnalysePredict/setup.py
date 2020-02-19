dcb6fc8f9db139895e9d46a3b8c25f7fdc5feabf
Marcus Moeng <marcusmoeng@yahoo.com>
2020-02-19 15:17:19 +0200

    Updating the setup.py file

--- AnalysePredict/setup.py
+++ AnalysePredict/setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+#with open("README.md", "r") as fh:
+    #long_description = fh.read()
 
 setuptools.setup(
     name="AnalysePredict-pkg",
@@ -9,7 +9,7 @@ setuptools.setup(
     author="Team_18_JHB_2020",
     author_email="marcusmoeng@yahoo.com",
     description="This package contains metric functions",
-    long_description=long_description,
+    #long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Team-18-JHB-Cohort/Analyse",
     packages=setuptools.find_packages(),
